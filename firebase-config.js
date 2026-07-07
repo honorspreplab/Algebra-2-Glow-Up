80585bff4a14c9b3d4076c926f947e5df0f4f44c
radhika <radhikatchandra@outlook.com>
2026-07-06 20:50:49 -0700

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,23 @@
+// Firebase setup file.
+// 1) Create a Firebase project.
+// 2) Enable Authentication providers: Google and Microsoft.
+// 3) Enable Firestore.
+// 4) Paste your Firebase web app config below.
+//
+// Keep OpenAI API keys OUT of this file. OpenAI keys belong only in Firebase
+// Functions config/secrets on the backend.
+
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDE3M-xOc-pjq6hvV2q3ERxT-cInsdjxTA",
+  authDomain: "honors-algebra-2-prep.firebaseapp.com",
+  projectId: "honors-algebra-2-prep",
+  storageBucket: "honors-algebra-2-prep.firebasestorage.app",
+  messagingSenderId: "67498976624",
+  appId: "1:67498976624:web:8eb966e6e9c099fca0dbb5"
+};
+
+window.APP_ADMIN_EMAILS = [
+  "radhikatchandra@outlook.com",
+  "radhikachandra809@gmail.com",
+  "ranveer.chandra@gmail.com"
+];
