f358755877644747fdb1a0aa7585d68634dc3203
radhika <radhikatchandra@outlook.com>
2026-07-07 18:35:15 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "algebra-glow-up-v28";
+const CACHE = "algebra-glow-up-v29";
 const APP_FILES = ["./", "./index.html", "./login.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", event => {
